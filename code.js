111761cd7b9bc2046566b8d2eca992f1cc92ada9
Charles Brant-Stec <charlesbrantstec@gmail.com>
2020-06-24 10:39:43 -0400

    eight tests

--- code.js
+++ code.js
@@ -1,10 +1,18 @@
 // helloWorld function
-function helloWorld() {
-    return "Hello, World!";
-}
+// function helloWorld() {
+//     return "Hello, World!";
+// }
 // sayHello function
 function sayHello(input){
-    return "Hello, " + input + "!";
+    // if (input === undefined) {
+    //     return "Hello, World!";
+    if (input === true || input === false || input === undefined){
+        return "Hello, World!";
+    // } else if (input === false){
+    //     return "Hello, World!";
+    } else {
+        return "Hello, " + input + "!";
+    }
     // if (input==="Alex") {
     //     return "Hello, Alex!";
     // } else if (input==="Pat") {
--- tests.js
+++ tests.js
@@ -27,10 +27,24 @@ describe('sayHello', function() {
     it('should return the string "Hello, Alex!" when executed', function() {
         expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
-    it('should return the string "Hello, Alex!" when executed', function() {
+    it('should return the string "Hello, Pat!" when executed', function() {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
-    it("should never return 'undefined' when called", function() {
-        expect(helloWorld()).not.toBe(undefined);
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(false)).toBe("Hello, World!")
     });
 });
+
+
+
+
+
+
+
+
